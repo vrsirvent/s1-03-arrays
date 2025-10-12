af3ba567bf90f0945f27ad1591e9d052710905d1
Vicenç Sirvent <vrsirvent@gmail.com>
2025-10-12 23:16:33 +0200

    Add and update level-1 and exercise-4

---
+++ level-1/exercise-4/README.md
@@ -0,0 +1,10 @@
+# S1-03-Arrays 
+  Level 1 - Exercise 4
+
+## Description 
+Create an associative array that represents information about yourself. Specifically, it must include:
+    name
+    age
+    email
+    favorite food
+
---
+++ level-1/exercise-4/src/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Associative array
+$personalInfo = array(
+    "name" => "Vicenç", 
+    "age" => 60, 
+    "email" => "vrsirvent@dominio.com", 
+    "favoriteFood" => "Rice" 
+);
+
+// Output
+echo "\nMy name: " . $personalInfo["name"] . "\n";
+echo "My age: " . $personalInfo["age"] . "\n";
+echo "My email: " . $personalInfo["email"] . "\n";
+echo "My favorite food: " . $personalInfo["favoriteFood"] . "\n\n";
+?>
+
