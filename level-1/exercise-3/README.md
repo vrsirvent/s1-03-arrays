05b3b83945e2adcf4dca9784d6ec574ffc2e27a0
Vicenç Sirvent <vrsirvent@gmail.com>
2025-10-12 23:16:15 +0200

    Add and update level-1 and exercise-3

---
+++ level-1/exercise-3/README.md
@@ -0,0 +1,10 @@
+# S1-03-Arrays 
+  Level 1 - Exercise 3
+
+## Description 
+Create a function that receives an array of words and a character as parameters. 
+The function returns true if all the words in the array have the character passed as the second parameter.
+
+For example: 
+If we have [“hello”, “Php”, “Html”], it will return true if we ask for ‘h’ but false if we ask for “l”.
+
---
+++ level-1/exercise-3/src/index.php
@@ -0,0 +1,37 @@
+<?php
+
+// Function
+function containsCharacter($words, $character) {
+    foreach ($words as $word) { // Iterate through each word in the array
+        $found = false; // Variable
+        $index = 0; // Initialize
+        while (!$found && $index < strlen($word)) { // Loop through each character in the word
+            if (strtolower($word[$index]) === strtolower($character)) {
+                $found = true; // If character is found
+            }
+            $index++;
+        }
+
+        if (!$found) { // If the character was not found in the word
+            return false;
+        }
+    }
+
+    return true; // If all words contain the character
+}
+
+// Variables
+$wordList = ["hola", "Php", "Html"];
+$character1 = "h";
+$character2 = "l";
+
+// Function call
+$result1 = containsCharacter($wordList, $character1);
+$result2 = containsCharacter($wordList, $character2);
+
+// Output
+echo "\nAll words contain the character '$character1'? " . ($result1 ? "Yes" : "No") . "\n";
+echo "All words contain the character '$character2'? " . ($result2 ? "Yes" : "No") . "\n";
+
+?>
+
