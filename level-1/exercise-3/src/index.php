1a857401e7281ed1d1f744c82538a04a96191534
Vicenç Sirvent <vrsirvent@gmail.com>
2025-10-19 23:47:15 +0200

    Update index.php file

--- level-1/exercise-3/src/index.php
+++ level-1/exercise-3/src/index.php
@@ -1,7 +1,8 @@
 <?php
 
 // Function
-function containsCharacter($words, $character) {
+// function containsCharacter($words, $character) { // sin refactorizar
+function containsCharacter(array $words, string $character): bool { // Refactorizar
     foreach ($words as $word) { // Iterate through each word in the array
         $found = false; // Variable
         $index = 0; // Initialize
@@ -32,6 +33,11 @@ $result2 = containsCharacter($wordList, $character2);
 // Output
 echo "\nAll words contain the character '$character1'? " . ($result1 ? "Yes" : "No") . "\n";
 echo "All words contain the character '$character2'? " . ($result2 ? "Yes" : "No") . "\n";
+
+// Ouput - un altra opció, codi Rubén
+// foreach($personalInfo as $key => $value) {
+//        echo $key." ".$value.PHP_EOL;
+// }
 
 ?>
 
